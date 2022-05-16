1ee6fcb5578e24fb8543cf40c366119c95b4081d
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-16 15:12:25 -0400

    update

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+// const logger = require('./middleware/logger');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -23,7 +28,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});

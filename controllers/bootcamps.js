0208ebbc4771ffdbca1c8083a151a1538fab9542
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-17 11:39:48 -0400

    controller added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@des    Get all bootcamps
 //@route  GET /api/v1/bootcamps
 //@access  Public
@@ -19,8 +21,17 @@ exports.getBootcamp = (req, res, next) => {
 //@des    Create new bootcamp
 //@route  POST /api/v1/bootcamps
 //@access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des    Update bootcamp

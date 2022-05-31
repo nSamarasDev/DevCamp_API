9ae888ba668cebcdfcd469fc68556b2a42c3b4b8
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-31 14:51:29 -0400

    cookie baker

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,9 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 
   // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 //@des    Login user
@@ -48,7 +46,27 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   // Create token
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from  modle, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, //  only accessed trough the client side script
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};

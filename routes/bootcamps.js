1a4b4c92c594d2818d03b152a93df25c3cd36f6a
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-31 21:43:51 -0400

    bootcamp routes update

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,31 +8,31 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
-
 const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
-
 // Include other resource routers
 const courseRouter = require('./courses');
 
 const router = express.Router();
+
+const { protect } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(createBootcamp);
-
-router.route('/:id/photo').put(bootcampPhotoUpload);
+  .post(protect, createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
 module.exports = router;

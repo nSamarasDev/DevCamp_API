f1fb09ad2ea3b01023fefad619e69a547eb367e2
Nicholas Samaras <n.samaras4@outlook.com>
2022-07-15 09:56:19 -0400

    logout user route added

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   resetPassword,
   updatedDetails,
   updatePassword,
+  logoutUser,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logoutUser);
 router.get('/me', protect, getMe);
 router.put('/updateddetails', protect, updatedDetails);
 router.put('/updatepassword', protect, updatePassword);
